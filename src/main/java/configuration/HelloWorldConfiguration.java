84c132195a538d97497ef15c193c4f0ee0c6b8cd
MalsR <malinda_rajapakse@hotmail.com>
2014-07-21 22:40:23 +0100

    created configuration and application file, application file sucks in configuration file which reads app details off yaml file

---
+++ src/main/java/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,34 @@
+package configuration;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class HelloWorldConfiguration extends Configuration {
+
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "unclassified";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String defaultName) {
+        this.defaultName = defaultName;
+    }
+}
