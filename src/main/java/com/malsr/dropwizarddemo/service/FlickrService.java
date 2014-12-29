a7da81ace2becd07c71948c69addf983bfe933c9
MalsR <malinda_rajapakse@hotmail.com>
2014-12-29 16:25:39 +0000

    Added logging to FlickrService.

--- src/main/java/com/malsr/dropwizarddemo/service/FlickrService.java
+++ src/main/java/com/malsr/dropwizarddemo/service/FlickrService.java
@@ -3,10 +3,14 @@ package com.malsr.dropwizarddemo.service;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.MediaType;
 
 public class FlickrService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FlickrService.class);
 
     private Client jerseyClient;
 
@@ -21,6 +25,8 @@ public class FlickrService {
         ClientResponse clientResponse = webResource
                 .accept(MediaType.TEXT_HTML)
                 .get(ClientResponse.class);
+
+        LOG.debug("Calling flickr endpoint and received response {}", clientResponse.getHeaders());
 
         return clientResponse.toString();
     }
